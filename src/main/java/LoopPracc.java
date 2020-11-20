72af9dd68bcc130d84d2487e1476c5b6e7e0121d
LeserHUN <bszab100@gmail.com>
2020-11-20 07:43:45 +0100

    Gyakorlas/házi 7:43

---
+++ src/main/java/LoopPracc.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class LoopPracc {
+
+    public static void main(String[] args) {
+        System.out.println("3.Feldatad:");
+
+        Scanner nev = new
+    }
+
+}
