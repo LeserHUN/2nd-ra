d5fb6a2b97c17459e65fe5de85cdcdb57acd3416
LeserHUN <bszab100@gmail.com>
2020-12-01 19:21:27 +0100

    19:21 --> Testek v.2

---
+++ src/main/java/com/github/leser/dependency/Consumer.java
@@ -0,0 +1,7 @@
+package com.github.leser.dependency;
+
+public class Consumer {
+    public void consume(String input){
+        System.out.println(input);
+    }
+}
---
+++ src/main/java/com/github/leser/dependency/Provider.java
@@ -0,0 +1,7 @@
+package com.github.leser.dependency;
+
+public class Provider {
+    public String get(String valami){
+        return "valami";
+    }
+}
