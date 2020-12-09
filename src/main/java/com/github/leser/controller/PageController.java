824ad22f2d390ad882dcaa9a5f3addd4358fc711
LeserHUN <bszab100@gmail.com>
2020-12-09 19:31:59 +0100

    19:31 --> Elkér + használ

--- src/main/java/com/github/leser/controller/PageController.java
+++ src/main/java/com/github/leser/controller/PageController.java
@@ -1,9 +1,12 @@
 package com.github.leser.controller;
 
+import com.github.leser.request.UsernameRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -12,5 +15,12 @@ public class PageController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+    @PostMapping("/input")
+    public ModelAndView input(UsernameRequest request){
+        log.info("/input - request: {}", request);
+        ModelAndView mav = new ModelAndView("input");
+        mav.addObject("username", request.getUsername());
+        return mav;
     }
 }
---
+++ src/main/java/com/github/leser/request/UsernameRequest.java
@@ -0,0 +1,10 @@
+package com.github.leser.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class UsernameRequest {
+    private String username;
+}
