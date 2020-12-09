8604b38c51a5cd864659678709281be16aab45b4
LeserHUN <bszab100@gmail.com>
2020-12-09 19:04:22 +0100

    19:04 --> Hello World!

---
+++ src/main/java/com/github/leser/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.github.leser.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
